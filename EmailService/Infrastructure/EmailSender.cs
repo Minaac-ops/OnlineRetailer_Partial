eba93aacd7f291979263c10837847fd01cd8e6fd
Minaac-ops <72027505+Minaac-ops@users.noreply.github.com>
2023-04-20 22:21:33 +0200

    g

--- EmailService/Infrastructure/EmailSender.cs
+++ EmailService/Infrastructure/EmailSender.cs
@@ -11,28 +11,18 @@ namespace EmailService.Infrastructure
     public class EmailSender : IEmailSender
     {
         private readonly EmailConfig _emailConfig;
-        private readonly EdgeFeatureHubConfig _featureHubConfig;
         
         public EmailSender(EmailConfig emailConfig)
         {
             _emailConfig = emailConfig;
-            _featureHubConfig = new EdgeFeatureHubConfig("http://localhost:8085","a54098bc-b44b-4b07-95ae-ba5df4d147d8/s4M0YTSp3vHP56yoFsFJdI1u9vp1cvDwONfdBZAa");
         }
 
         public async Task SendEmail(Message message)
         {
-            var fh = await _featureHubConfig.NewContext().Build();
-            if (fh["SendEmailFeature"].IsEnabled)
-            {
-                var emailMessage = await CreateEmailMessage(message);
+            var emailMessage = await CreateEmailMessage(message);
                 Console.WriteLine(message.To);
                 await Send(emailMessage);
             }
-            else
-            {
-                throw new NotImplementedException();
-            }
-        }
 
         private async Task<MimeMessage> CreateEmailMessage(Message message)
         {
