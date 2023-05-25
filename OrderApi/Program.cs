c254c766517a1fb298bdadfadd4e675ed48e8f6d
Minaac-ops <72027505+Minaac-ops@users.noreply.github.com>
2023-05-25 22:15:29 +0200

    Update Program.cs

--- OrderApi/Program.cs
+++ OrderApi/Program.cs
@@ -64,7 +64,6 @@ builder.Services.AddControllers().AddDapr();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme).AddCertificate();
 
 var app = builder.Build();
 
